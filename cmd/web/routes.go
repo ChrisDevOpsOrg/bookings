ecffc6faa61c0cf92ec49473496a839c58c4a693
Chris <zhichen.wang@1103@gmail.com>
2023-05-07 10:22:30 +0800

    use go-chi library

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,15 +3,19 @@ package main
 import (
 	"github.com/ChrisDevOpsOrg/bookings/internal/config"
 	"github.com/ChrisDevOpsOrg/bookings/internal/handlers"
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 	"net/http"
 )
 
 func routes(app *config.AppConfig) http.Handler {
-	mux := pat.New()
+	mux := chi.NewRouter()
 
-	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	// middleware allows you to process a request as it comes into you Web application
+	// and perform some action on it.
+	mux.Use(middleware.Recoverer)
 
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
 	return mux
 }
